f23410f7fb9b8f4f472f3cf887c9646986e1c222
LipeDC <felipinhodias75@gmail.com>
2025-06-23 17:18:27 -0300

    Solucao ruim

--- application/Program.java
+++ application/Program.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) throws ParseException {
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         System.out.print("Room number: ");
         int number = sc.nextInt();
@@ -18,11 +18,10 @@ public class Program {
         System.out.print("Check-out date (dd/MM/yyyy): ");
         Date checkOut = sdf.parse(sc.next());
 
-        if (!checkOut.after(checkIn)){
+        if (!checkOut.after(checkIn)) {
             System.out.println("Error in reservation: Check-out must be after check-in date");
-        }
-        else {
-            Reservation reservation = new Reservation(number,checkIn,checkOut);
+        } else {
+            Reservation reservation = new Reservation(number, checkIn, checkOut);
             System.out.println("Reservation: " + reservation);
 
             System.out.println();
@@ -32,18 +31,13 @@ public class Program {
             System.out.print("Check-out date (dd/MM/yyyy): ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if(checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            }
-            else if (!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out must be after check-in date");
-            }
-            else {
-                reservation.updateDates(checkIn, checkOut);
+            String error = reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
+            } else {
                 System.out.println("Reservation: " + reservation);
             }
         }
-           sc.close();
+        sc.close();
     }
-}
+    }
